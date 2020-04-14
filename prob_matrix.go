7d6b3464594230c3486005234be18fa0746e8772
Nuno <nm.morais@campus.fct.unl.pt>
2020-04-14 14:53:54 +0100

    add auto client with probability matrix

---
+++ prob_matrix.go
@@ -0,0 +1,13 @@
+package main
+
+
+
+var Matrix = [][]float32{
+	/* challenge, Queue, Trade, Store, Catch */    
+	/* c,   q,    t,    s,    c*/
+	{0.05, 0.05, 0.30, 0.20, 0.40}, // previousMove = Challenge
+	{0.05, 0.05, 0.30, 0.20, 0.40}, // previousMove = Queue
+	{0.05, 0.05, 0.30, 0.20, 0.40}, // previousMove = Trade
+	{0.05, 0.05, 0.30, 0.20, 0.40}, // previousMove = Buy
+	{0.05, 0.05, 0.30, 0.20, 0.40}, // previousMove = Catch
+}
