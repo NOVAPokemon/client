8858875e95f79b6fc83e7f21d0f9f7382b2aaf06
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-27 18:30:46 +0100

    fix prob matrix missing line for microtransactions

--- prob_matrix.go
+++ prob_matrix.go
@@ -2,9 +2,10 @@ package main
 
 var Matrix = [][]float32{
 	/* ChallengeCmd, QueueCmd, MakeMicrotransactionCmd, TradeCmd, StoreCmd, CatchCmd, RaidCmd */
-	/* c,   q,    m,,  t,    s,    c,	r*/
+	/* c,   q,    m,,  t,    s,    c,	 r*/
 	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = Challenge
 	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = Queue
+	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = MakeMicrotransactionCmd
 	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = Trade
 	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = Store
 	{0.05, 0.05, 0.10, 0.20, 0.20, 0.30, 0.10}, // previousMove = Catch
