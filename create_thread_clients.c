61f19bde76e7ba6687d2922fcd248599a45ef28e
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-06-01 15:39:39 +0100

    fix typo

--- create_thread_clients.c
+++ create_thread_clients.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include<unistd.h>
+#include <unistd.h>
 
 void *run_client(void *vargp) {
     char *args[]={"./$executable", "-a", NULL};
